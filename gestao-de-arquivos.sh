5200ee669f94fddfa8fd5025f3757a9f16392a5a
Willian Alves Fonseca <willian.alves2109@gmail.com>
2023-12-10 12:09:47 -0300

    Alterando script de gestão de arquivos

--- gestao-de-arquivos.sh
+++ gestao-de-arquivos.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
 #############################################
-# Documentação do Script
+#
+# DOCUMENTAÇÃO DO SCRIPT
 # Este script gerencia arquivos de acordo com os parâmetros fornecidos.
 # - Função 'criar_arquivos': Cria arquivos com nome base, tipo e quantidade especificados.
 # - Função 'deletar_arquivos': Deleta arquivos que correspondem ao termo de busca fornecido.
 # - Função 'renomear_arquivos': Renomeia arquivos que contêm o termo de busca no nome.
+#
 #############################################
-
-#!/bin/bash
 
 # Função para criar arquivos
 criar_arquivos() {
-  nome_base=$1
-  tipo=$2
-  quantidade=$3
+  NOME_BASE="$1"
+  TIPO="$2"
+  QUANTIDADE="$3"
 
-  for i in $(seq 1 $quantidade); do
-    touch "${nome_base}${i}.${tipo}"
-    echo "Arquivo criado: ${nome_base}${i}.${tipo}"
+  for i in $(seq 1 $QUANTIDADE); do
+    touch "${NOME_BASE}${i}.${TIPO}"
+    echo "Arquivo criado: ${NOME_BASE}${i}.${TIPO}"
   done
 }
 
 # Função para deletar arquivos
 deletar_arquivos() {
-  termo_busca=$1
-  arquivos_encontrados=$(find . -type f -name "*${termo_busca}*")
+  TERMO_BUSCA="$1"
+  ARQUIVOS_ENCONTRADOS=$(find . -type f -name "*${TERMO_BUSCA}*")
 
   echo "Arquivos selecionados para exclusão:"
-  echo "${arquivos_encontrados}"
+  echo "${ARQUIVOS_ENCONTRADOS}"
 
-  read -p "Confirmar exclusão? (s/n): " confirmacao
-  if [[ $confirmacao == "s" ]]; then
-    echo "${arquivos_encontrados}" | xargs rm -f
+  read -p "Confirmar exclusão? (s/n): " CONFIRMACAO
+  if [[ $CONFIRMACAO == "s" ]]; then
+    echo "${ARQUIVOS_ENCONTRADOS}" | xargs rm -f
     echo "Arquivos excluídos."
   else
     echo "Tarefa abortada. Informe o arquivo correto em uma próxima tentativa."
@@ -41,36 +41,39 @@ deletar_arquivos() {
 
 # Função para renomear arquivos
 renomear_arquivos() {
-  termo_busca=$1
-  novo_nome=$2
-  arquivos_encontrados=$(find . -type f -name "*${termo_busca}*")
+  TERMO_BUSCA="$1"
+  NOVO_NOME="$2"
+  CONT=1
+
+  ARQUIVOS_ENCONTRADOS=$(find . -type f -name "*${TERMO_BUSCA}*")
 
   echo "Arquivos encontrados para renomear:"
-  echo "${arquivos_encontrados}"
+  echo "${ARQUIVOS_ENCONTRADOS}"
 
-  read -p "Confirmar renomeação? (s/n): " confirmacao
-  if [[ $confirmacao == "s" ]]; then
-    cont=1
-    for arquivo in ${arquivos_encontrados}; do
-      mv "${arquivo}" "${novo_nome}${cont}.${arquivo##*.}"
-      echo "Arquivo renomeado: ${novo_nome}${cont}.${arquivo##*.}"
-      ((cont++))
-    done
-  else
+  read -p "Confirmar renomeação? (s/n): " CONFIRMACAO
+  if [[ $CONFIRMACAO != "s" ]]; then
     echo "Tarefa abortada. Informe os parâmetros corretos em uma próxima tentativa."
+    return
   fi
+
+  echo "${ARQUIVOS_ENCONTRADOS}" | while read -r ARQUIVO; do
+    EXTENSAO="${ARQUIVO##*.}"
+    mv "$ARQUIVO" "${NOVO_NOME}${CONT}.${EXTENSAO}"
+    echo "Arquivo renomeado para: ${NOVO_NOME}${CONT}.${EXTENSAO}"
+    ((CONT++))
+  done
 }
 
 # Tratamento dos argumentos de linha de comando
 case $1 in
 "criar")
-  criar_arquivos $2 $3 $4
+  criar_arquivos "$2" $3 $4
   ;;
 "deletar")
-  deletar_arquivos $2
+  deletar_arquivos "$2"
   ;;
 "renomear")
-  renomear_arquivos $2 $3
+  renomear_arquivos "$2" "$3"
   ;;
 *)
   echo "Argumento inválido. Use 'criar', 'deletar' ou 'renomear'."
